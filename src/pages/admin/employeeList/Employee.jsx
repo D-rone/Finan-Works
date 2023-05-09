019a25a5c5d0d7d229e2ef4e7f407429895b76b1
Drone <medouksili@gmail.com>
2023-05-09 15:12:12 +0100

    added tbody on employee table

--- src/pages/admin/employeeList/Employee.jsx
+++ src/pages/admin/employeeList/Employee.jsx
@@ -1,39 +1,43 @@
 import React from "react";
 import "./Employee.css";
-import quit from "../../../assest/quitX.svg"
+import quit from "../../../assest/quitX.svg";
 
 export default function Employee({ data, handleProfileClick }) {
   return (
     <div id="shade">
       <div id="profileContainer">
-        <button onClick={handleProfileClick} id="quitBtn"><img src={quit} alt=""></img></button>
-        <h2>
-            Employee Information
-        </h2>
+        <button onClick={handleProfileClick} id="quitBtn">
+          <img src={quit} alt=""></img>
+        </button>
+        <h2>Employee Information</h2>
 
         <table>
+          <tbody>
             <tr>
-                <th>Picture : </th>
-                <td> <img src={data.image}/> </td>
+              <th>Picture : </th>
+              <td>
+                {" "}
+                <img src={data.image} />{" "}
+              </td>
             </tr>
             <tr>
-                <th>Name :</th>
-                <td>{data.name}</td>
+              <th>Name :</th>
+              <td>{data.name}</td>
             </tr>
             <tr>
-                <th>Email : </th>
-                <td>{data.email}</td>
+              <th>Email : </th>
+              <td>{data.email}</td>
             </tr>
             <tr>
-                <th>Type</th>
-                <td>{data.type}</td>
+              <th>Type</th>
+              <td>{data.type}</td>
             </tr>
             <tr>
-                <th>N° Tel :</th>
-                <td>{data.phone}</td>
+              <th>N° Tel :</th>
+              <td>{data.phone}</td>
             </tr>
+          </tbody>
         </table>
-      
       </div>
     </div>
   );
